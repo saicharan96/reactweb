9392ca0f1e097bba52fd34f3b79e649eb8b8ed94
saicharan <saicharan14996@gmail.com>
2019-07-14 11:12:55 +0530

    dd
    dddd

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,27 @@ import {Helmet} from "react-helmet";
 const About = () => 
 {
     return(
-<div className="container">
+<body>
   <Helmet><title>About Me</title></Helmet>
-  <div class="card top">
-    <h4><b>About</b></h4> 
-    <p>A passionate digital marketer expertise in building SEO friendly responsive websites and optimize according to the business needs to drive traffic in different search engines of different countries like Google,Yandex, Microsoft Bing, Baidu, Yahoo Gemini.</p> 
-<br/>
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
- </div>
+  <div className="about">
+   <div className="container">
+     <div className="row">
+       <div className="col-md-6">
+   <h3>About Me</h3>
+   <p>A passionate digital marketer expertise in building SEO friendly responsive websites and optimize according to the business needs to drive traffic in different search engines like Google,Yandex, Microsoft Bing, Baidu, Yahoo and improves the business searches and visibilty using Paid Ads with ultimate result towards ROI.</p>
+   <br/>
+   <p>My interest towards Digital Marketing arised when i was pursuing my Engineering, while my friends moved towards development side learning new stuffs. I was bit confused about where should my career path moving on, but still i sticked with digital marketing and started exploring all the products on digital marketing. Trying all the softwares and tools to gain more knowledge on how exactly it works.</p>
+    <p><b>Email: saiseotechie@gmail.com</b></p>
+    <p><b> Phone: + 91 9551936390</b></p>
+   <button className="buttonabout">Contact Me</button>
+   </div>
+   <div className="col-md-6">
+   <img className="aboutimage"  id="borderimg2" src={require('../assets/sai.jpg')} />
+     </div>
+    </div> </div></div>
+
   
-  </div>
+  </body>
     );
 };
 
